9016c12bd1d26f097c4452af2598b934df686882
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-08-06 23:38:49 +0530

    added

--- new1.py
+++ new1.py
@@ -1,15 +1,32 @@
 #fibonacci series starts from 0,1
 
-n=int(input("enter a number: "))
-a=0
-b=1
-print(a)
-print(b)
+# n=int(input("enter a number: "))
+# a=0
+# b=1
+# print(a)
+# print(b)
 
-for i in range(n):
-    c=a+b
-    a=b
-    b=c
-    print(c)
+# for i in range(n):
+#     c=a+b
+#     a=b
+#     b=c
+#     print(c)
 
-    
+# using recursion
+print(0)
+print(1)
+count = 2
+
+def fibonacci(prev1, prev2):
+    global count
+    if count <= 19:
+        newFibo = prev1 + prev2
+        print(newFibo)
+        prev2 = prev1
+        prev1 = newFibo
+        count += 1
+        fibonacci(prev1, prev2)
+    else:
+        return
+
+fibonacci(1,0)
