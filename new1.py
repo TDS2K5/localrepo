603dcd9717fd3ce75f3a2ee3237e3c342ca97d91
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-08-08 06:15:07 +0530

    ds

--- new1.py
+++ new1.py
@@ -13,20 +13,77 @@
 #     print(c)
 
 # using recursion
-print(0)
-print(1)
-count = 2
+# print(0)
+# print(1)
+# count = 2
 
-def fibonacci(prev1, prev2):
-    global count
-    if count <= 19:
-        newFibo = prev1 + prev2
-        print(newFibo)
-        prev2 = prev1
-        prev1 = newFibo
-        count += 1
-        fibonacci(prev1, prev2)
-    else:
-        return
+# def fibonacci(prev1, prev2):
+#     global count
+#     if count <= 19:
+#         newFibo = prev1 + prev2
+#         print(newFibo)
+#         prev2 = prev1
+#         prev1 = newFibo
+#         count += 1
+#         fibonacci(prev1, prev2)
+#     else:
+#         return
 
-fibonacci(1,0)
+# fibonacci(1,0)
+
+# nth fibonacci using recursion
+# def F(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return F(n - 1) + F(n - 2)
+
+# n=int(input("enter a number: "))
+# print(F(n))
+
+# # 
+# print(0)
+# print(1)
+# count = 2
+
+# def fibonacci(prev1, prev2,n):
+#     global count
+#     if count <= n:
+#         newFibo = prev1 + prev2
+#         print(newFibo)
+#         prev2 = prev1
+#         prev1 = newFibo
+#         count += 1
+        
+#         fibonacci(prev1, prev2,n)
+#     else:
+#         return
+
+# n=int(input("enter a number: "))
+# fibonacci(1,0,n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
