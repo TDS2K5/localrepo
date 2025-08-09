bfbc30cc25cf4b1ac32f8a316e05956db2b6e7e8
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-08-09 07:17:14 +0530

    dsa

--- selection.py
+++ selection.py
@@ -3,9 +3,25 @@
 # Go through the array again as many times as there are values in the array.
 
 arr=[]
-n=int(input("enter array size: "))
-for s in range(n):
+l=int(input("enter array size: "))
+for s in range(l):
     ai=int(input(f"enter element {s}: "))
     arr.append(ai)
-print(arr)
+
+n=len(arr)
+
+# for i in range(n-1):
+#     mindex=i
+#     for j in range(i+1,n):
+#         if arr[j]<arr[mindex]:
+#             mindex=j
+    
+#     arr[i],arr[mindex]=arr[mindex],arr[i]
+
+# print(arr)
+
+
+
+
+
 
